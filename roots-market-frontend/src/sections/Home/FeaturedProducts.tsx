898ecffde250a7672d86934a0422b9c3dfda1736
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-11 00:15:48 -0500

    feat: update FeatureProducts section

--- roots-market-frontend/src/sections/Home/FeaturedProducts.tsx
+++ roots-market-frontend/src/sections/Home/FeaturedProducts.tsx
@@ -1,12 +1,14 @@
 import { CardProduct } from "../../components/UI"
-import { useProductRanking } from "../../hooks/useProduct"
+import { Product } from "../../models/product.model"
 
-export function FeaturedProducts(){
-  const products = useProductRanking()
+interface Props {
+  products: Product[]
+}
 
+export function FeaturedProducts({ products }: Props){
   return(
     <section
-      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8"
+      className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-4 gap-8"
     >
       {products.map((product) => (
         <CardProduct key={product.id} product={product} />
