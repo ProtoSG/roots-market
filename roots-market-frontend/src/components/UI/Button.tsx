0d6704248255030f7990b3f2076202e9b8484d60
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-11 00:07:51 -0500

    feat: add Button component

--- roots-market-frontend/src/components/UI/Button.tsx
+++ roots-market-frontend/src/components/UI/Button.tsx
@@ -1,17 +1,33 @@
+import clsx from "clsx"
 import { ReactNode } from "react"
 
 interface Props{
   children: ReactNode
   onClick: () => void
+  className?: string
 }
 
-export function Button({children, onClick}: Props){
+export function PrimaryButton({children, onClick, className}: Props){
   return(
     <button
-      className={` 
-        bg-primary m-auto rounded py-2 px-4 text-white transition-all focus:outline-none
-        hover:bg-red-700 hover:cursor-pointer hover:scale-105 hover:drop-shadow-2xl
-      `}
+      className={clsx(` 
+        bg-primary border border-primary rounded-lg py-2 px-4 text-white transition-all focus:outline-none
+        hover:bg-red-700 hover:cursor-pointer
+      `, className)}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
+
+export function SecondaryButton({children, onClick, className}: Props) {
+  return (
+    <button
+      className={clsx(`
+        border border-primary text-primary rounded-lg py-2 px-4 transition-all focus:outline-none
+        hover:bg-zinc-500 hover:border-zinc-500 hover:text-white hover:cursor-pointer
+      `, className)}
       onClick={onClick}
     >
       {children}
