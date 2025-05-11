f3887dfd1735f24da64cc926063f3bf6a69f77f0
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-11 00:14:41 -0500

    feat: update Artisans page

--- roots-market-frontend/src/pages/Artisans.tsx
+++ roots-market-frontend/src/pages/Artisans.tsx
@@ -1,5 +1,26 @@
+import { ChangeEvent, useState } from "react";
+import { MainContainer, SearchInput } from "../components/UI";
+import { ListArtisans } from "../sections/Artisans";
+import { ModalArtisan } from "../components/Artisans";
+
 export function Artisans(){
+  const [ searchWord, setSearchWord ] = useState("")
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target
+    if( name === "search") setSearchWord(value)
+  }
+
   return(
-    <p>Artisans</p>
+    <MainContainer className="flex-col gap-8">
+      <SearchInput
+        name="search"
+        value={searchWord}
+        onChange={handleChange}
+        placeholder="Buscar artesano..."
+      />
+      <ListArtisans searchWord={searchWord} />
+      <ModalArtisan />
+    </MainContainer>
   )
 }
