45ce1e67bb3be9f539b6369c3aa66c26dd78ae6b
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-11 00:08:09 -0500

    feat: update CardProduct component

--- roots-market-frontend/src/components/UI/CardProduct.tsx
+++ roots-market-frontend/src/components/UI/CardProduct.tsx
@@ -1,28 +1,30 @@
 import { Product } from "../../models/product.model"
-import { useProductDialogStore } from "../../stores/dialogStore"
+import { useDialogStore } from "../../stores/dialogStore"
+import { useProductStore } from "../../stores/itemStore"
 
 interface Props {
   product: Product
 }
 
 export function CardProduct({product}: Props){
-  const {setOpen, setItem} = useProductDialogStore()
+  const { setOpen } = useDialogStore()
+  const { setItem } = useProductStore()
 
   const handleOpenDialog = () => {
-    setOpen()
+    setOpen(true)
     setItem(product)
   }
 
   return(
     <article
-      className="flex flex-col gap-4 items-center transition-all duration-300 group hover:cursor-pointer hover:scale-105 hover:drop-shadow-2xl hover:drop-shadow-red-950/40"
+      className="z-0 flex flex-col gap-4 items-center transition-all duration-300 group  hover:cursor-pointer hover:scale-105 hover:drop-shadow-2xl hover:drop-shadow-red-950/40"
       onClick={handleOpenDialog}
     >
       <div className="w-full h-64 rounded bg-zinc-200">
-        <img src={product.images[0]} className="w-full h-full object-cover rounded" />
+        <img src={product.images[0]} className="w-full h-full object-cover rounded-lg" />
       </div>
     <div className="">
-        <h3 className="text-lg">{product.name}</h3>
+        <h3 className="text-lg text-center text-pretty">{product.name}</h3>
         <p className="text-center">S/ {product.price.toFixed(2)}</p>
       </div>
     </article>
