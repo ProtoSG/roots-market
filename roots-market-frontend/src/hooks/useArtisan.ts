6b2e88bcd3ad3b302eb83eb44c88effc5c96ea06
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-11 00:11:00 -0500

    feat: update useArtisan hook (add useArtisans)

--- roots-market-frontend/src/hooks/useArtisan.ts
+++ roots-market-frontend/src/hooks/useArtisan.ts
@@ -1,5 +1,5 @@
-import { artisanAdapter } from "../adapters/artisan.adapter"
-import { ArtisanTestimony, ArtisanTestimonyResponse } from "../models/artisan.model"
+import { artisanAdapter, artisanTestimonyAdapter } from "../adapters/artisan.adapter"
+import { Artisan, ArtisanResponse, ArtisanTestimony, ArtisanTestimonyResponse } from "../models/artisan.model"
 import { fetchData } from "../utils/fetchData"
 import { useQuery } from "./useQuery"
 
@@ -7,10 +7,22 @@ export const useLastArtisan = () => {
   const data = useQuery({
     fn: () => fetchData<ArtisanTestimony, ArtisanTestimonyResponse>({
       url: 'artisan/last',
-      jsonAdapter: artisanAdapter
+      jsonAdapter: artisanTestimonyAdapter
     }),
     key: 'artisanLast'
   })
 
   return data[0]
+}
+
+export const useArtisans = () => {
+  const artisans = useQuery({
+    fn: () => fetchData<Artisan, ArtisanResponse>({
+      url: 'artisan',
+      jsonAdapter: artisanAdapter
+    }),
+    key: 'artisans'
+  })
+
+  return artisans
 }
