8fce4d3f9593bfa1d156d78b6ae1557a322fda5d
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-11 00:03:57 -0500

    feat: add artisan adapter

--- roots-market-frontend/src/adapters/artisan.adapter.ts
+++ roots-market-frontend/src/adapters/artisan.adapter.ts
@@ -1,8 +1,18 @@
-import { ArtisanTestimony, ArtisanTestimonyResponse,  } from "../models/artisan.model";
+import { Artisan, ArtisanResponse, ArtisanTestimony, ArtisanTestimonyResponse,  } from "../models/artisan.model";
+import { socialNetworkAdapter } from "./socialNetwork.adapter";
 
-export const artisanAdapter = (artisan: ArtisanTestimonyResponse): ArtisanTestimony => ({
+export const artisanTestimonyAdapter = (artisan: ArtisanTestimonyResponse): ArtisanTestimony => ({
   id: artisan.artisanId,
   name: artisan.name,
   testimony: artisan.testimony,
   profileImageURL: artisan.profileImageUrl
+})
+
+export const artisanAdapter = (artisan: ArtisanResponse): Artisan => ({
+  id: artisan.artisanId,
+  name: artisan.name,
+  bio: artisan.bio,
+  location: artisan.location,
+  profileImageURL: artisan.profileImageUrl,
+  socialNetworks: artisan.socialNetworks.map((st) => socialNetworkAdapter(st))
 })
