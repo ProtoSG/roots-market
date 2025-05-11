bfc4ce907c07aca2a55889d309e820e0a36b70b0
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-11 00:04:54 -0500

    feat: add ItemSocialNetwork component

---
+++ roots-market-frontend/src/components/Artisans/ItemSocialNetwork.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react"
+import { FacebookIcon, InstagramIcon, YoutubeIcon } from "../../icons"
+import { SocialNetwork } from "../../models/socialNetwork.model"
+import { UnderlineText } from "../UI"
+
+interface Props {
+  item: SocialNetwork
+}
+
+const ICONS: Record<string, ReactNode> = {
+  instagram: <InstagramIcon />,
+  facebook: <FacebookIcon />,
+  youtube: <YoutubeIcon />
+}
+
+export function ItenSocialNetwork({item}: Props){
+  const icon = ICONS[item.type.toLowerCase()]
+
+  return (
+    <a 
+      href={item.url}
+      target="_blank"
+      className="flex gap-3 text-sm items-center focus:outline-none transition-all hover:text-primary hover:underline"
+    >
+      {icon}
+      {item.type}
+    </a>
+  )
+}
