1d95bc61806a812d05da30e66f287c49f900c4e6
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-11 00:08:27 -0500

    feat: update Header component

--- roots-market-frontend/src/components/UI/Header.tsx
+++ roots-market-frontend/src/components/UI/Header.tsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom"
-import { ShoppingIcon } from "../../icons"
+import { useEffect, useRef, useState } from "react"
 import { LinkItem } from "./LinkItem"
+import { ShoppingButton } from "./ShoppingButton"
+import { ShoppingCart } from "./ShoppingCart"
 
 interface LinkItem {
   name: string
@@ -23,25 +24,24 @@ const navLinks: LinkItem[] = [
 ]
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false)
+  const cartRef = useRef<HTMLDivElement>(null)
+
   return(
-    <header className="flex justify-between pt-6 pb-12">
-      <nav>
-        <ul className="flex gap-7">
-          {navLinks.map(({name, path}) => (
-            <LinkItem key={path} name={name} path={path} />
-            ))}
-        </ul>
-      </nav>
-      <div className="relative hover:cursor-pointer group">
-        <ShoppingIcon className="size-8 group-hover:text-primary transition-colors"/>
-        <div
-          className="absolute -right-1 -bottom-1 flex items-center justify-center size-5 rounded-full bg-primary"
-        >
-          <small
-            className="text-white"
-          >1</small>
-        </div>
-      </div>
+    <header className="flex mx-auto justify-center items-center">
+      <section className="flex fixed top-0 w-full max-w-5xl z-50 justify-between py-6 px-4 sm:px-16  backdrop-blur-lg">
+        <nav>
+          <ul className="flex gap-7">
+            {navLinks.map(({name, path}) => (
+              <LinkItem key={path} name={name} path={path} />
+              ))}
+          </ul>
+        </nav>
+        <ShoppingButton  setIsOpen={setIsOpen}/>
+        {isOpen && 
+          <ShoppingCart isOpen={isOpen} setIsOpen={setIsOpen} cartRef={cartRef} />
+        }
+      </section>
     </header>
   )
 }
