b06736e2e3cc3af4d6fdf91ac9f65890b99492c1
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-11 00:14:23 -0500

    feat: update socialNetwork

--- roots-market-frontend/src/models/socialNetwork.model.ts
+++ roots-market-frontend/src/models/socialNetwork.model.ts
@@ -1,13 +1,19 @@
 import { z } from "zod";
 
-export const socialNetworkSchema = z.object({
-  socialNetworkId: z.number().optional(),
-  artisanId: z.number().int(),
+export const baseSocialNetworkSchema = z.object({
   type: z.string().min(1, "El tipo de red social es obligatorio"),
   url: z.string().url("La URL de la red social debe ser válida"),
 });
 
-export const socialNetworkUpdateSchema = z.object({
-  type: z.string().min(1, "El tipo de red social es obligatorio"),
-  url: z.string().url("La URL de la red social debe ser válida"),
+export const socialNetworkSchema = baseSocialNetworkSchema.extend({
+  id: z.number().int() 
 })
+
+export type BaseSocialNetwork = z.infer<typeof baseSocialNetworkSchema>
+export type SocialNetwork = z.infer<typeof socialNetworkSchema>
+
+export type SocialNetworkResponse = {
+  socialNetworkId: number
+  type: string
+  URL: string
+}
