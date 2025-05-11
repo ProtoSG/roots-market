078ed1560b42487505abe6dab5e325622120535c
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-11 00:08:18 -0500

    feat: add DialogContainer component

---
+++ roots-market-frontend/src/components/UI/DialogContainer.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from "react"
+import { useDialog } from "../../hooks/useDialog"
+import { ArrowIcon } from "../../icons"
+import { useDialogStore } from "../../stores/dialogStore"
+
+interface Props{
+  children: ReactNode
+  fn?: () => void
+}
+
+export function DialogContainer({children, fn}: Props){
+  const { isOpen, setOpen } = useDialogStore()
+  const dialogRef = useDialog(isOpen)
+
+  const handleClose = () => {
+    setOpen(false)
+    if (fn) fn()
+  }
+
+  return(
+    <dialog 
+      ref={dialogRef}      
+      onClose={handleClose}
+      className="m-auto w-full sm:max-w-4xl lg:max-w-4xl backdrop:backdrop-blur-sm backdrop:backdrop-brightness-90 focus:outline-none animate-fade-in"
+    >
+      <section
+        className="relative px-14 pb-16 pt-20 flex flex-col gap-8"
+      >
+        <span
+          onClick={handleClose}
+          className="absolute top-8 transition-all hover:cursor-pointer hover:text-primary hover:scale-125"
+        >
+          <ArrowIcon />
+        </span>
+        {children}
+      </section>
+    </dialog>
+  )
+}
