ef515e270a1e53c2c836b5e8b7691f1aabce5775
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-11 00:13:12 -0500

    feat: update Toaster in Layout component

--- roots-market-frontend/src/layouts/Layout.tsx
+++ roots-market-frontend/src/layouts/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/UI";
+import { Toaster } from "sonner";
 
 export default function Layout(){
   return (
     <>
       <Header />
       <Outlet />
+      <Toaster richColors/>
     </>
   )
 }
