77968eb0937dddd610105e05ea8ecdea377cb73d
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-17 12:42:54 -0500

    fix: change model artisan, use typeof schema

--- roots-market-backend/src/models/artisan.model.ts
+++ roots-market-backend/src/models/artisan.model.ts
@@ -1,33 +1,4 @@
-export class Artisan{
-  artisanId?: number;
-  name: string;
-  username: string;
-  password: string;
-  bio: string;
-  location: string;
-  profileImageUrl: string;
-  email: string;
-  createdAt: Date;
+import type { z } from "zod";
+import type { artisanSchema } from "../schemas/artisan.schema";
 
-  constructor(
-    name: string,
-    username: string,
-    password: string,
-    bio: string,
-    location: string,
-    profileImageUrl: string,
-    email: string,
-    createdAt: Date,
-    artisanId?: number,
-  ){
-    this.artisanId = artisanId
-    this.name = name
-    this.username = username
-    this.password = password
-    this.bio = bio
-    this.location = location
-    this.profileImageUrl = profileImageUrl
-    this.email = email
-    this.createdAt = createdAt
-  }
-}
+export type Artisan = z.infer<typeof artisanSchema>
