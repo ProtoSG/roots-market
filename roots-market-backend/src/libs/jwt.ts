f90da888c72a2d1b3d205376eeab3674080fa0ae
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-05-17 12:42:12 -0500

    fix: update the type of the id parameter

--- roots-market-backend/src/libs/jwt.ts
+++ roots-market-backend/src/libs/jwt.ts
@@ -1,11 +1,11 @@
-import jwt from "jsonwebtoken"
-import { TOKEN_SECRET } from "../config.ts"
+import jwt from "jsonwebtoken";
+import { TOKEN_SECRET } from "../config.ts";
 
-export async function createAccessToken(payload: {id: bigint | undefined}) {
+export async function createAccessToken(payload: { id: number | undefined }): Promise<string> {
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, TOKEN_SECRET, {expiresIn: "1d"}, (err: Error, token: string) => {
-      if (err) reject(err);
-      resolve(token)
-    })
-  })
+    jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" }, (err: Error | null, token?: string) => {
+      if (err || !token) return reject(err);
+      resolve(token);
+    });
+  });
 }
