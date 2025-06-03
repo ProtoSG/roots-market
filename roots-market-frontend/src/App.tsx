53b5ecf50016466136540edc1bf29ef21a426226
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-03 17:52:29 -0500

    feat: update App component

--- roots-market-frontend/src/App.tsx
+++ roots-market-frontend/src/App.tsx
@@ -1,14 +1,23 @@
 import { Route, Routes } from "react-router-dom"
-import { Artisans, Home, Products } from "./pages"
-import Layout from "./layouts/Layout"
+import { ArtisanInfo, ArtisanProducts, Artisans, Cart, Home, Products } from "./pages"
+import { ProtectedRoute } from "./ProtectedRoute"
+import { Layout, ArtisanLayout } from "./layouts"
 
 export default function App() {
   return (
     <Routes>
-      <Route path="" element={<Layout />} >
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/artisans" element={<Artisans />} />
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="artisans" element={<Artisans />} />
+        <Route path="shopping-cart" element={<Cart />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="artisan" element={<ArtisanLayout />} >
+            <Route index element={<ArtisanInfo />} />
+            <Route path="products" element={<ArtisanProducts />}/>
+          </Route>
+        </Route>
       </Route>
     </Routes>
   )
