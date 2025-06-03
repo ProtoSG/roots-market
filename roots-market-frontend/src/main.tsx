1d936aaf3fbefbcaa40fc974ad64bef1a58b988d
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-03 17:52:39 -0500

    feat: addd AuthProvider in main

--- roots-market-frontend/src/main.tsx
+++ roots-market-frontend/src/main.tsx
@@ -4,15 +4,18 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ErrorBoundary } from 'react-error-boundary'
+import { AuthProvider } from './context/auth.context.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <ErrorBoundary fallbackRender={({error}) => <div>Error: {error.message}</div>}>
+        <AuthProvider>
           <BrowserRouter>
             <Suspense fallback={<div>Loading...</div>}>
                 <App />
             </Suspense>
           </BrowserRouter>
+        </AuthProvider>
       </ErrorBoundary>
   </StrictMode>,
 )
