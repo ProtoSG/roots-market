b246f7a14ad9b3b1f79c682ccb8ef0224740138b
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-03 17:50:19 -0500

    feat: update ListArtisans section

--- roots-market-frontend/src/sections/Artisans/ListArtisans.tsx
+++ roots-market-frontend/src/sections/Artisans/ListArtisans.tsx
@@ -12,9 +12,9 @@ export function ListArtisans({searchWord}: Props) {
   const artisansFilter = artisans.filter((art) => art.name.toLocaleLowerCase().includes(searchWord.toLowerCase()))
 
   return (
-    <section className="grid grid-cols-1  md:grid-cols-2 gap-12">
+    <section className="grid grid-cols-1 rounded-lg  md:grid-cols-2 lg:grid-cols-3 gap-12">
       {artisansFilter.map((artisan) => (
-        <CardArtisan artisan={artisan} />
+        <CardArtisan key={artisan.id} artisan={artisan} />
       ))}
     </section>
   )
