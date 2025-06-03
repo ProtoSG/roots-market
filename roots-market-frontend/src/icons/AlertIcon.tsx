765fd36edcdb4dd664bdb864c4bb6189a6a64a7c
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-03 17:48:37 -0500

    feat: add new Icons svg

---
+++ roots-market-frontend/src/icons/AlertIcon.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from "./icon.model";
+
+export function AlertIcon({className}: IconProps){
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" /><path d="M12 8v4" /><path d="M12 16h.01" />
+    </svg>
+  )
+}
---
+++ roots-market-frontend/src/icons/CheckIcon.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from "./icon.model";
+
+export function CheckIcon({className}: IconProps){
+  return(
+    <svg 
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" />
+    </svg>
+  )
+}
