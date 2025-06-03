861323de6ac6f473c99b596d3b61deb9c7c779d1
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-03 17:46:41 -0500

    feat: update app.ts

--- roots-market-backend/src/app.ts
+++ roots-market-backend/src/app.ts
@@ -2,17 +2,14 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
-import artisanRoutes from "./routes/artisan.routes.ts"
-import categoryRoutes from "./routes/category.routes.ts"
-import imageRoutes from "./routes/image.routes.ts"
-import orderRoutes from "./routes/order.routes.ts"
-import orderDetailRoutes from "./routes/orderDetail.routes.ts"
-import productRoutes from "./routes/product.routes.ts"
-import socialNetworkRoutes from "./routes/socialNetwork.routes.ts"
-import tagRoutes from "./routes/tag.routes.ts"
-import authRoutes from "./routes/auth.routes.ts"
 import { FRONTEND_URL } from "./config.ts"
 import { swaggerSpec, swaggerUI } from "./swagger.ts"
+
+import artisanRoutes from "./routes/artisan.routes.ts"
+import categoryRoutes from "./routes/category.routes.ts"
+import orderRoutes from "./routes/order.routes.ts"
+import productRoutes from "./routes/product.routes.ts"
+import authRoutes from "./routes/auth.routes.ts"
 
 const app = express()
 
@@ -28,13 +25,9 @@ app.use(cookieParser())
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
 app.use("/api", artisanRoutes)
-app.use("/api", imageRoutes)
 app.use("/api", categoryRoutes)
 app.use("/api", orderRoutes)
-app.use("/api", orderDetailRoutes)
 app.use("/api", productRoutes)
-app.use("/api", socialNetworkRoutes)
-app.use("/api", tagRoutes)
 app.use("/api/auth", authRoutes)
 
 export default app
