53193a96131ad8791ac4a4764137b146042ba815
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-24 03:35:22 -0500

    feat: update main

--- roots-market-frontend/src/main.tsx
+++ roots-market-frontend/src/main.tsx
@@ -4,18 +4,28 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ErrorBoundary } from 'react-error-boundary'
-import { AuthProvider } from './context/auth.context.tsx'
+import { AuthProvider } from './context/auth.provider.tsx'
+import ErrorBackPage from './ErrorBackPage.tsx'
+import { ProductProvider } from './context/product.provider.tsx'
+import { CategoryProvider } from './context/category.provider.tsx'
+import { PublicArtisanProvider } from './context/publicArtisan.provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <ErrorBoundary fallbackRender={({error}) => <div>Error: {error.message}</div>}>
-        <AuthProvider>
-          <BrowserRouter>
-            <Suspense fallback={<div>Loading...</div>}>
-                <App />
-            </Suspense>
-          </BrowserRouter>
-        </AuthProvider>
-      </ErrorBoundary>
+    <ErrorBoundary fallbackRender={({error}) => <ErrorBackPage error={error.message} />}>
+      <AuthProvider>
+        <ProductProvider>
+          <PublicArtisanProvider>
+            <CategoryProvider>
+              <BrowserRouter>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <App />
+                </Suspense>
+              </BrowserRouter>
+            </CategoryProvider>
+          </PublicArtisanProvider>
+        </ProductProvider>
+      </AuthProvider>
+    </ErrorBoundary>
   </StrictMode>,
 )
