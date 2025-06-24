f2f1ed32e1a4cc800f2056b1c798a01848823fe0
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-06-24 03:34:24 -0500

    feat: update artisan page

--- roots-market-frontend/src/pages/Home.tsx
+++ roots-market-frontend/src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import { MainContainer } from "../components/UI";
-import { useProductRanking } from "../hooks/useProduct";
+import { useProduct } from "../hooks/useProduct";
 import { Hero, FeaturedProducts, ArtisanHighlight } from "../sections/Home";
 
 export function Home(){
-  const products = useProductRanking()
-  
+  const {productsRanking} = useProduct()
+
   return(
     <MainContainer className="flex-col gap-24 ">
       <Hero />
-      <FeaturedProducts products={products} />
+      <FeaturedProducts products={productsRanking} />
       <ArtisanHighlight />
     </MainContainer>
   )
--- roots-market-frontend/src/pages/artisan/ArtisanInfo.tsx
+++ roots-market-frontend/src/pages/artisan/ArtisanInfo.tsx
@@ -1,5 +1,14 @@
+import { useArtisanPrivate } from "../../hooks/useArtisan"
+import { CardAllInfoArtisan } from "../../sections/Artisan/ArtisanInfo";
+import { ModaInfoArtisan } from "../../sections/Artisan/ArtisanInfo/ModalInfoArtisan";
+
 export function ArtisanInfo(){
+  const {artisanInfo} = useArtisanPrivate()
+
   return (
-    <p>Artisan Info</p>
+    <section className="w-full min-h-full flex items-center justify-center">
+      <CardAllInfoArtisan artisan={artisanInfo} />
+      <ModaInfoArtisan />
+    </section>
   )
 }
