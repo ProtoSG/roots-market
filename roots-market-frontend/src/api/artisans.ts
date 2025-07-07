faa19354fbbe8e7cff16c29bfac4d1d815b41216
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-07-07 10:01:49 -0500

    feat: add more funtions in artisan api

--- roots-market-frontend/src/api/artisans.ts
+++ roots-market-frontend/src/api/artisans.ts
@@ -1,5 +1,9 @@
-import { artisanAdapter, artisanTestimonyAdapter } from "../adapters/artisan.adapter";
-import { Artisan, ArtisanLast, ArtisanLastResponse, ArtisanPagination, ArtisanPaginationResponse, ArtisanResponse } from "../models/artisan.model";
+import { artisanAdapter, artisanTestimonyAdapter, artisanUpdateAdapter } from "../adapters/artisan.adapter";
+import { productAdapter } from "../adapters/product.adapter";
+import { Artisan, ArtisanLast, ArtisanLastResponse, ArtisanPagination, ArtisanPaginationResponse, ArtisanResponse, ArtisanUpdate, ArtisanUpdateRequest } from "../models/artisan.model";
+import { ProductFilter, ProductResponse } from "../models/product.model";
+import { ResponseWritter } from "../models/response.model";
+import { BaseSocialNetwork, SocialNetwork } from "../models/socialNetwork.model";
 import { requestJSON } from "./requestJSON"
 
 export const getLastArtisan = async(): Promise<ArtisanLast> => {
@@ -74,4 +78,88 @@ export const getArtisanInfo = async(): Promise<Artisan> =>{
   }
 
   return artisanAdapter(data)
+}
+
+export const putArtisanInfo = async(artisan: ArtisanUpdate) => {
+  const artisanAdapted = artisanUpdateAdapter(artisan)
+
+  const {status, data} = await requestJSON<ResponseWritter>("/artisans/me", {
+    method: "PUT",
+    credentials: "include",
+    body: JSON.stringify(artisanAdapted)
+  })
+
+  if (status < 200 || status >= 300) {
+    throw new Error(`Failed to fetch artisans: ${status}`);
+  }
+
+  if (data === null) {
+    throw new Error("No data received for artisans");
+  }
+
+  return data
+}
+
+export const createSocialNetwork = async (sn: BaseSocialNetwork) => {
+  const { status, data } = await requestJSON<ResponseWritter>("/artisans/me/socialNetworks", {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify(sn)
+  })
+
+
+  if (status < 200 || status >= 300) {
+    throw new Error(`Failed to fetch artisans: ${status}`);
+  }
+
+  if (data === null) {
+    throw new Error("No data received for social network");
+  }
+
+  return data
+}
+
+export const updateSocialNetwork = async (id: number, sn: BaseSocialNetwork) => {
+  const { status, data } = await requestJSON<ResponseWritter>(`/artisans/me/socialNetworks/${id}`, {
+    method: "PUT",
+    credentials: "include",
+    body: JSON.stringify({
+      "type": sn.type,
+      "url": sn.url,
+    })
+  })
+
+  if (status < 200 || status >= 300) {
+    throw new Error(`Failed to fetch artisans: ${status}`);
+  }
+
+  if (data === null) {
+    throw new Error("No data received for social network");
+  }
+
+  return data
+}
+
+export const getProductsArtisan = async (): Promise<ProductFilter> => {
+  const { status, data }= await requestJSON<ProductResponse>("/artisans/me/products", {
+    method: "GET",
+    credentials: "include"
+  })
+
+  if (status < 200 || status >= 300) {
+    throw new Error(`Failed to fetch artisans: ${status}`);
+  }
+
+  if (data === null) {
+    throw new Error("No data received for social network");
+  }
+
+  const products = data.data.map(productAdapter)
+
+  const dataAdap = {
+    ...data,
+    data: products
+  }
+
+  return dataAdap;
 }
