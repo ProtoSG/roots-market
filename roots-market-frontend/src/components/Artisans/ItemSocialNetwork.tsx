ea626fcf8fb07c02d910df146e6c5826a1cea856
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-07-07 10:02:23 -0500

    feat: update component

--- roots-market-frontend/src/components/Artisans/ItemSocialNetwork.tsx
+++ roots-market-frontend/src/components/Artisans/ItemSocialNetwork.tsx
@@ -1,25 +1,26 @@
 import { ReactNode } from "react"
-import { FacebookIcon, InstagramIcon, YoutubeIcon } from "../../icons"
-import { SocialNetwork } from "../../models/socialNetwork.model"
+import { FacebookIcon, InstagramIcon, TiktokIcon, YoutubeIcon } from "../../icons"
+import { listSocialNetworks, SocialNetwork } from "../../models/socialNetwork.model"
 
 interface Props {
   item: SocialNetwork
 }
 
-const ICONS: Record<string, ReactNode> = {
-  instagram: <InstagramIcon />,
-  facebook: <FacebookIcon />,
-  youtube: <YoutubeIcon />
+const ICONS: Record<listSocialNetworks, ReactNode> = {
+  [listSocialNetworks.INSTAGRAM]: <InstagramIcon />,
+  [listSocialNetworks.FACEBOOK]: <FacebookIcon />,
+  [listSocialNetworks.YOUTUBE]: <YoutubeIcon />,
+  [listSocialNetworks.TIKTOK]: <TiktokIcon />
 }
 
 export function ItemSocialNetwork({item}: Props){
-  const icon = ICONS[item.type.toLowerCase()]
+  const icon = ICONS[item.type]
 
   return (
     <a 
       href={item.url}
       target="_blank"
-      className="flex gap-3 text-sm items-center focus:outline-none transition-all hover:text-primary hover:underline"
+      className="flex basis-1/6 gap-3 text-sm items-center focus:outline-none transition-all hover:text-primary hover:underline"
     >
       {icon}
       {item.type}
