c09eeb076a59fb5fedaa71bef2b0fe0051321cf0
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-07-17 13:00:18 -0500

    feat: update protected route

--- roots-market-frontend/src/ProtectedRoute.tsx
+++ roots-market-frontend/src/ProtectedRoute.tsx
@@ -1,10 +1,17 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "./hooks/useAuth";
 
 export const ProtectedRoute = () => {
   const { isAuthenticated, loading } = useAuth();
+  const location = useLocation();
+
+  if (isAuthenticated && location.pathname === "/artisan") {
+    console.log("PASA?", isAuthenticated)
+    return <Navigate to="/artisan" replace />;
+  }
 
   if (loading) return <h1>Loading...</h1>;
-  if (!isAuthenticated && !loading) return <Navigate to="/" replace />;
+  if (!isAuthenticated) return <Navigate to="/" replace />;
+
   return <Outlet />;
 };
